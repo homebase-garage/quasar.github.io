14c99e4b7f5fd620e4076a04d49ceca690d0b4b5
Razvan Stoenescu <razvan.stoenescu@gmail.com>
2026-03-30 17:56:19 +0300

    feat(cli): "serve" cmd > replace express with h3

--- cli/lib/cmd/serve.js
+++ cli/lib/cmd/serve.js
@@ -4,29 +4,24 @@ const argv = parseArgs(process.argv.slice(2), {
   alias: {
     p: 'port',
     H: 'hostname',
-    g: 'gzip',
     s: 'silent',
     colors: 'colors',
-    o: 'open',
-    c: 'cache',
     cors: 'cors',
-    m: 'micro',
-    history: 'history',
+    o: 'open',
     i: 'index',
+    sw: 'sw',
+    history: 'history',
     https: 'https',
     C: 'cert',
     K: 'key',
     P: 'proxy',
     h: 'help'
   },
-  boolean: ['g', 'https', 'colors', 'history', 'h', 'cors'],
-  string: ['H', 'C', 'K', 'i'],
+  boolean: ['https', 'colors', 'history', 'https', 'cors'],
+  string: ['H', 'C', 'K', 'i', 'sw'],
   default: {
     p: process.env.PORT || 4000,
     H: process.env.HOSTNAME || '0.0.0.0',
-    g: true,
-    c: 24 * 60 * 60,
-    m: 1,
     i: 'index.html',
     colors: true
   }
@@ -47,29 +42,22 @@ if (argv.help) {
   Options
     --port, -p              Port to use (default: 4000)
     --hostname, -H          Address to use (default: 0.0.0.0)
-    --gzip, -g              Compress content (default: true)
     --silent, -s            Suppress log message
     --colors                Log messages with colors (default: true)
+    --cors                  Enable CORS
     --open, -o              Open browser window after starting
-    --cache, -c <number>    Cache time (max-age) in seconds;
-                            Does not apply to /service-worker.js
-                            (default: 86400 - 24 hours)
-    --micro, -m <seconds>   Use micro-cache (default: 1 second)
 
-    --history               Use history api fallback;
+    --sw <path>             Service worker url path (default: sw.js)
+    --index, -i <path>      Index url path (default: index.html)
+    --history               Use history mode;
                               All requests fallback to /index.html,
                               unless using "--index" parameter
-    --index, -i <file>      History mode (only!) index url path
-                              (default: index.html)
+                              (default: false)
 
     --https                 Enable HTTPS
     --cert, -C [path]       Path to SSL cert file (Optional)
     --key, -K [path]        Path to SSL key file (Optional)
-    --proxy <file.mjs>      Proxy specific requests defined in file;
-                            File must export Array ({ path, rule })
-                            See example below. "rule" is defined at:
-                            https://github.com/chimurai/http-proxy-middleware
-    --cors                  Enable CORS for all requests
+
     --help, -h              Displays this message
 
   Proxy file example
@@ -84,154 +72,138 @@ if (argv.help) {
   process.exit(0)
 }
 
-import { readFileSync, existsSync } from 'node:fs'
-import path from 'node:path'
+import { existsSync } from 'node:fs'
+import { stat, readFile } from 'node:fs/promises'
+import { join, isAbsolute } from 'node:path'
 
 import { cliPkg } from '../cli-pkg.js'
 import { log, fatal } from '../logger.js'
+import { defineEventHandler } from 'h3'
 
 const root = getAbsolutePath(argv._[0] || '.')
-const resolve = p => path.resolve(root, p)
+const resolve = p => join(root, p)
 
 function getAbsolutePath(pathParam) {
-  return path.isAbsolute(pathParam)
-    ? pathParam
-    : path.join(process.cwd(), pathParam)
+  return isAbsolute(pathParam) ? pathParam : join(process.cwd(), pathParam)
 }
 
 if (!argv.colors) {
   process.env.FORCE_COLOR = '0'
 }
 
-const { default: express } = await import('express')
 const { green, gray, red } = await import('kolorist')
+const { H3, serve, serveStatic, onResponse, onError } = await import('h3')
 
-const resolvedIndex = resolve(argv.index)
-const microCacheSeconds = argv.micro ? parseInt(argv.micro, 10) : false
-
-const serve = (servePath, cache) => {
-  const opts = {
-    maxAge: cache ? parseInt(argv.cache, 10) * 1000 : 0,
-    setHeaders(res, headersPath) {
-      if (res.req.method === 'GET' && headersPath === resolvedIndex) {
-        res.set(
-          'Cache-Control',
-          'no-store, no-cache, must-revalidate, proxy-revalidate'
-        )
-        res.set('Pragma', 'no-cache')
-        res.set('Expires', '0')
-        res.set('Surrogate-Control', 'no-store')
-      }
-    }
-  }
-
-  if (argv.history !== true) {
-    opts.index = argv.index
-  }
-
-  return express.static(resolve(servePath), opts)
-}
-
-const app = express()
+const app = new H3()
 
 if (argv.cors) {
-  const { default: cors } = await import('cors')
-  app.use(cors())
+  const { handleCors } = await import('h3')
+  app.use(
+    defineEventHandler(event => {
+      handleCors(event, {
+        origin: '*',
+        preflight: {
+          statusCode: 204
+        },
+        methods: '*'
+      })
+    })
+  )
 }
 
 if (!argv.silent) {
-  app.get('{*path}', (req, _, next) => {
-    console.log(
-      `GET ${green(req.url)} ${gray('[' + req.ip + ']')} ${new Date()}`
-    )
-    next()
+  app.use(
+    onResponse((response, event) => {
+      console.log(
+        `${green(`[${event.req.method}]`)} ${event.url.pathname} ${green(response.status)} ${gray('[' + event.req.ip + ']')} ${new Date()}`
+      )
+    })
+  )
+
+  app.use(
+    onError((error, event) => {
+      console.log(
+        `${red(`[${event.req.method}]`)} ${event.url.pathname} ${red(`!! ${error.message}`)} ${gray('[' + event.req.ip + ']')} ${new Date()}`
+      )
+    })
+  )
+}
+
+const swFile = resolve(argv.sw || 'sw.js')
+if (!existsSync(swFile)) {
+  if (argv.sw) {
+    fatal(`Service worker file not found: ${swFile}`)
+  }
+} else {
+  const swFileContent = await readFile(swFile, 'utf-8')
+
+  app.use(event => {
+    if (event.req.method !== 'GET') return
+    if (event.url.pathname !== `/${argv.sw}`) return
+
+    event.res.status = 200
+    event.res.statusText = 'OK'
+    event.res.headers.set('Content-Type', 'application/javascript')
+
+    return swFileContent
   })
-}
-
-if (argv.gzip) {
-  const { default: compression } = await import('compression')
-  app.use(compression({ threshold: 0 }))
-}
-
-const serviceWorkerFile = resolve('service-worker.js')
-if (existsSync(serviceWorkerFile)) {
-  app.use('/service-worker.js', serve('service-worker.js'))
 }
 
 if (argv.proxy) {
   let file = (argv.proxy = getAbsolutePath(argv.proxy))
   if (!existsSync(file)) {
-    fatal('Proxy definition file not found! ' + file)
+    fatal(`Proxy definition file not found: ${file}`)
   }
+
   file = await import(file)
 
+  const { fromNodeMiddleware } = await import('h3')
   const { createProxyMiddleware } = await import('http-proxy-middleware')
 
   ;(file.default || file).forEach(entry => {
-    app.use(entry.path, createProxyMiddleware(entry.rule))
+    app.use(entry.path, fromNodeMiddleware(createProxyMiddleware(entry.rule)))
   })
 }
 
 if (argv.history) {
-  const { default: history } = await import('connect-history-api-fallback')
-  app.use(
-    history({
-      index: argv.index.startsWith('/') ? argv.index : '/' + argv.index
-    })
-  )
-}
-
-app.use('/', serve('.', true))
-
-if (microCacheSeconds) {
-  const { default: microcache } = await import('route-cache')
-  app.use(microcache.cacheSeconds(microCacheSeconds))
-}
-
-app.get('{*path}', (req, res) => {
-  res.setHeader('Content-Type', 'text/html')
-  res.status(404).send('404 | Page Not Found')
-  if (!argv.silent) {
-    console.log(red(`  404 on ${req.url}`))
-  }
-})
-
-const getServer = async localApp => {
-  if (!argv.https) return localApp
-
-  let fakeCert, key, cert
-
-  if (argv.key && argv.cert) {
-    key = getAbsolutePath(argv.key)
-    cert = getAbsolutePath(argv.cert)
-
-    if (existsSync(key)) {
-      key = readFileSync(key)
-    } else {
-      fatal('SSL key file not found!' + key)
-    }
-
-    if (existsSync(cert)) {
-      cert = readFileSync(cert)
-    } else {
-      fatal('SSL cert file not found!' + cert)
-    }
-  } else {
-    const { getCertificate } = await import('@quasar/ssl-certificate')
-    fakeCert = await getCertificate({ log, fatal })
+  const indexFile = resolve(argv.index)
+  if (!existsSync(indexFile)) {
+    fatal(`Index file not found: ${indexFile}`)
   }
 
-  const https = await import('node:https')
-  return https.createServer(
-    {
-      key: key || fakeCert,
-      cert: cert || fakeCert
-    },
-    localApp
-  )
+  const indexFileContent = await readFile(indexFile, 'utf-8')
+
+  app.use(event => {
+    if (event.req.method !== 'GET') return
+
+    const acceptHeader = event.req.headers.get('Accept')
+    if (!acceptHeader?.includes('text/html')) return
+    if (existsSync(resolve(event.url.pathname))) return
+
+    event.res.status = 200
+    event.res.statusText = 'OK'
+    event.res.headers.set('Content-Type', 'text/html')
+
+    return indexFileContent
+  })
 }
 
-const server = await getServer(app)
+app.use(event =>
+  serveStatic(event, {
+    indexNames: [`${argv.index.startsWith('/') ? '' : '/'}${argv.index}`],
+    encodings: argv.gzip ? { gzip: '.gz', br: '.br' } : {},
+    getContents: id => readFile(resolve(id)),
+    getMeta: async id => {
+      const stats = await stat(resolve(id)).catch(() => {})
+      if (stats?.isFile()) {
+        return {
+          size: stats.size,
+          mtime: stats.mtimeMs
+        }
+      }
+    }
+  })
+)
 
 const getListeningUrl = hostname =>
   `http${argv.https ? 's' : ''}://${hostname}:${argv.port}`
@@ -244,47 +216,88 @@ const getListeningBanner = () => {
   if (hostname === '0.0.0.0') {
     const acc = getIPs().map(ip => ['', getListeningUrl(ip)])
     if (acc.length !== 0) {
-      acc[0][0] = 'Listening at'
+      acc[0][0] = 'URLs in use'
       return acc
     }
 
     hostname = 'localhost'
   }
 
-  return [['Listening at', getListeningUrl(hostname)]]
+  return [['URL in use', getListeningUrl(hostname)]]
 }
 
-server.listen(argv.port, argv.hostname, async () => {
-  const filler = ''.padEnd(20, ' ')
-  const info = [
-    ['Quasar CLI', `v${cliPkg.version}`],
-    ...getListeningBanner(),
-    ['Web server root', root],
-    argv.https ? ['HTTPS', 'enabled'] : '',
-    argv.gzip ? ['Gzip', 'enabled'] : '',
-    ['Cache (max-age)', argv.cache || 'disabled'],
-    microCacheSeconds ? ['Micro-cache', microCacheSeconds + 's'] : '',
-    argv.history ? ['History mode', 'enabled'] : '',
-    ['Index file', argv.index],
-    argv.cors ? ['CORS', 'enabled'] : '',
-    argv.proxy ? ['Proxy definitions', argv.proxy] : ''
-  ]
-    .filter(msg => msg)
-    .map(
-      msg =>
-        ' ' +
-        (msg[0] !== '' ? msg[0].padEnd(20, '.') : filler) +
-        ' ' +
-        green(msg[1])
-    )
-
-  console.log('\n' + info.join('\n') + '\n')
-
-  if (argv.open) {
-    const { isMinimalTerminal } = await import('../is-minimal-terminal.js')
-    if (!isMinimalTerminal) {
-      const { default: open } = await import('open')
-      open(getListeningUrl(argv.hostname), { url: true })
+const getHttpOptions = async () => {
+  if (!argv.https) {
+    return {
+      protocol: 'http'
     }
   }
+
+  let fakeCert, key, cert
+
+  if (argv.key && argv.cert) {
+    key = getAbsolutePath(argv.key)
+    cert = getAbsolutePath(argv.cert)
+
+    if (existsSync(key)) {
+      key = await readFile(key)
+    } else {
+      fatal(`SSL key file not found: ${key}`)
+    }
+
+    if (existsSync(cert)) {
+      cert = await readFile(cert)
+    } else {
+      fatal(`SSL cert file not found: ${cert}`)
+    }
+  } else {
+    const { getCertificate } = await import('@quasar/ssl-certificate')
+    fakeCert = await getCertificate({ log, fatal })
+  }
+
+  return {
+    protocol: 'https',
+    tls: {
+      key: key || fakeCert,
+      cert: cert || fakeCert
+    }
+  }
+}
+
+const server = serve(app, {
+  port: argv.port,
+  hostname: argv.hostname,
+  ...(await getHttpOptions())
 })
+
+await server.ready()
+
+const filler = ''.padEnd(20, ' ')
+const info = [
+  ['Quasar CLI', `v${cliPkg.version}`],
+  ...getListeningBanner(),
+  ['Web server root', root],
+  argv.https ? ['HTTPS', 'enabled'] : '',
+  ['Index file', argv.index],
+  argv.history ? ['History mode', 'enabled'] : '',
+  argv.cors ? ['CORS', 'enabled'] : '',
+  argv.proxy ? ['Proxy definitions', argv.proxy] : ''
+]
+  .filter(msg => msg)
+  .map(
+    msg =>
+      ' ' +
+      (msg[0] !== '' ? msg[0].padEnd(20, '.') : filler) +
+      ' ' +
+      green(msg[1])
+  )
+
+console.log('\n' + info.join('\n') + '\n')
+
+if (argv.open) {
+  const { isMinimalTerminal } = await import('../is-minimal-terminal.js')
+  if (!isMinimalTerminal) {
+    const { default: open } = await import('open')
+    open(getListeningUrl(argv.hostname), { url: true })
+  }
+}
