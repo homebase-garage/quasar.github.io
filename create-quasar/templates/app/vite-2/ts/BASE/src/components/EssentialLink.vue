e26084e1f9882f17242c0f9fcca94797f57b4885
Razvan Stoenescu <razvan.stoenescu@gmail.com>
2026-05-18 11:38:07 +0300

    fix(create-quasar): TS templates title -> label

--- create-quasar/templates/app/vite-2/ts/BASE/src/components/EssentialLink.vue
+++ create-quasar/templates/app/vite-2/ts/BASE/src/components/EssentialLink.vue
@@ -13,7 +13,7 @@
     </q-item-section>
 
     <q-item-section>
-      <q-item-label>{{ title }}</q-item-label>
+      <q-item-label>{{ label }}</q-item-label>
       <q-item-label caption>{{ caption }}</q-item-label>
     </q-item-section>
   </q-item>
