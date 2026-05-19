ece7dffcae7ac5cdb9e2b5aaf057dc117ce3c414
Razvan Stoenescu <razvan.stoenescu@gmail.com>
2026-05-19 15:15:00 +0300

    feat(icongenie): support for q/app-vite v3

--- icongenie/lib/modes/v2/electron.js
+++ icongenie/lib/modes/v2/electron.js
@@ -1,23 +1,32 @@
+import { existsSync } from 'node:fs'
+import { resolveDir } from '../../utils/app-paths.js'
+
+const dir = existsSync(resolveDir('src-electron/electron-assets'))
+  ? 'electron-assets' // q/app-vite v3+
+  : existsSync(resolveDir('src-electron/icons'))
+    ? 'icons' // q/app-vite v2 or q/app-webpack v4
+    : 'electron-assets' // fallback to q/app-webpack v3 specs
+
 export default [
   {
     // macos (embedded icons)
     generator: 'icns',
     name: 'icon.icns',
-    folder: 'src-electron/icons'
+    folder: `src-electron/${dir}/icons`
   },
 
   {
     // windows (embedded icon)
     generator: 'ico',
     name: 'icon.ico',
-    folder: 'src-electron/icons'
+    folder: `src-electron/${dir}/icons`
   },
 
   {
     // tray icon (all platforms)
     generator: 'png',
     name: 'icon.png',
-    folder: 'src-electron/icons',
+    folder: `src-electron/${dir}/icons`,
     sizes: [512]
   }
 ]
